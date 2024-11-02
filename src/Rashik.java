15bf65cc2f489c5f2ad3cec61382ca1d8170bb0d
RashikTimalsina <rashiktimalsina21@gmail.com>
2024-11-02 17:32:41 +0545

    update  Rashik.java

--- src/Rashik.java
+++ src/Rashik.java
@@ -1,5 +1,6 @@
 public class Rashik {
     public static void main(String[] args) {
-        System.out.println("rashik timalsina");;
+        System.out.println("rashik timalsina");
+        System.out.println("i am don of imadol");
     }
 }
