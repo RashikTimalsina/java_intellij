3e93bc9d5810c689f210eaef17e51505bcbe2c37
RashikTimalsina <rashiktimalsina21@gmail.com>
2024-11-02 17:38:48 +0545

    add new package com.rashiktimalsina

---
+++ src/Divyagyan.java
@@ -0,0 +1,5 @@
+class Divyagyan {
+    public static void main(String[] args) {
+        System.out.println("Dybya gyan college");
+    }
+}
--- src/Rashik.java
+++
@@ -1,6 +0,0 @@
-public class Rashik {
-    public static void main(String[] args) {
-        System.out.println("rashik timalsina");
-        System.out.println("i am don of imadol");
-    }
-}
